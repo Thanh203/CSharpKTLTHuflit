2230488fb24dc8dd30b3b87c14354a2a3bb75b13
thanh <lmtthanh203@>
2022-03-16 11:56:25 +0700

    Done

--- Week2/BT2/Program.cs
+++ Week2/BT2/Program.cs
@@ -6,7 +6,36 @@ namespace BT2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int m, n, k;
+            int[,] a;
+            xulixau(out m,out n,out k,out a);
+            incotthuk(k,m,a);
+            Console.ReadKey();
+        }
+
+        public static void xulixau(out int m, out int n, out int k, out int[,] a)
+        {
+            var numberList = Console.ReadLine().Split(' ');
+            m = int.Parse(numberList[0]);
+            n = int.Parse(numberList[1]);
+            k = int.Parse(numberList[2]);
+            a = new int[m, n];
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                var numberList2 = Console.ReadLine().Split(' ');
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    a[i, j] = int.Parse(numberList2[j]);
+                }
+            }
+        }
+
+        public static void incotthuk(int k, int m, int[,] a)
+        {
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                Console.Write(a[i, k- 1]+ " ");
+            }
         }
     }
 }
