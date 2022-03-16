06d71e8af4a1f3b6152aab03dbc3d5b677a4f989
thanh <lmtthanh203@>
2022-03-16 13:31:48 +0700

    done

---
+++ Week2/BT4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BT4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void dulieuvao(out int[,] a)
+        {
+            
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                
+            }
+        }
+    }
+}
---
+++ Week2/BT5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BT5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int[,] a;
+            dulieuvao(out n, out a);
+            incc(n, a);
+            Console.ReadKey();
+        }
+
+        public static void dulieuvao(out int n, out int[,] a)
+        {
+            n = int.Parse(Console.ReadLine());
+            a = new int[n, n];
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                var numberList = Console.ReadLine().Split(' ');
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    a[i, j] = int.Parse(numberList[j]);
+                }
+            }
+            
+        }
+
+        public static void incc(int n, int[,] a)
+        {
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        Console.WriteLine(a[i, j]);
+                    }
+                }
+            }
+        }
+    }
+}
